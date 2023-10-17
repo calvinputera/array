0cb9ad4a9d7496709e663ff2383026ca6abbfb7b
calvinputeraloka <calvinputeraloka@gmail.com>
2023-10-17 20:41:29 +0700

    array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [dataPost, setDataPost] = useState([]);
+	const [dataFilter, setDataFilter] = useState(dataPost);
+
+	useEffect(() => {
+		const getPost = async () => {
+			const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+				.then((resp) => resp.json())
+				.then((resp) => {
+					setDataPost(resp);
+					setDataFilter(resp);
+				});
+		};
+		getPost();
+	}, []);
+
+	const filtered = (e) => {
+		setDataFilter(
+			dataPost.filter((val) => val.title.toLowerCase().includes(e.target.value))
+		);
+	};
+
+	const totalPrice = data.reduce((total, menu) => {
+		return total + menu.price;
+	}, 0);
+
+	return (
+		<div className="container">
+			<div className="section-one">
+				<input type="text" placeholder="filter.." onChange={filtered} />
+
+				{dataFilter.map((item, index) => (
+					<div className="post" key={index}>
+						<p className="post-title">{item.title}</p>
+						<p className="post-desc">{item.body}</p>
+					</div>
+				))}
+			</div>
+			<div className="section-two">
+				<p>Total Harga: Rp {totalPrice}</p>
+				{data.map((item, index) => (
+					<div className="post" key={index}>
+						<p className="post-title">{item.product}</p>
+						<p className="post-desc">{item.price}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const data = [
+	{
+		id: 1,
+		product: "Nasi Goreng",
+		price: 20000,
+	},
+	{
+		id: 2,
+		product: "Mie Goreng",
+		price: 18000,
+	},
+	{
+		id: 3,
+		product: "Bihun Goreng",
+		price: 15000,
+	},
+	{
+		id: 4,
+		product: "Ayam Goreng",
+		price: 25000,
+	},
+	{
+		id: 5,
+		product: "Ikan Goreng",
+		price: 22000,
+	},
+];
